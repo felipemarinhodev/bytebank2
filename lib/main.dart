e2bd3ab075df01ebed2e0de498feb341a6a74afa
Felipe Marinho <felipmarinho@gmail.com>
2020-02-23 14:57:12 -0300

    Adding image of the network

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,13 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard')),
+        appBar: AppBar(title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.network('https://cdn.pixabay.com/photo/2012/03/03/23/06/backdrop-21534_1280.jpg')
+          ],
+        ),
       ),
     );
   }
