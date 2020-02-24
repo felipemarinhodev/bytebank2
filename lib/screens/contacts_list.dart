7f4b7b791dcf672719842a1f6552b282e130c349
Felipe Marinho <felipmarinho@gmail.com>
2020-02-24 06:36:46 -0300

    Ending the flow of the App

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
@@ -22,7 +23,14 @@ class ContactsList extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          ).then((newContact) => debugPrint(newContact.toString()),
+          );
+        },
         child: Icon(
           Icons.add,
         ),
